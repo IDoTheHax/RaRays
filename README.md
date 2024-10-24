fd932c253bbd02bf5c64ec505ee61382912db9b0
IDoTheHax <hax@idothehax.com>
2024-10-24 15:04:26 +0200

    init project

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ra's Rays
+
+This is a server side mod that adds orbital strike cannons to the game
+with the use of block display entitys and polymer.
+
+> This mod is heavily inspired by Cymaera's SkyTorch and would of existed
+without him
---
+++ src/main/java/net/idothehax/rarays/RaRays.java
@@ -0,0 +1,15 @@
+package net.idothehax.rarays;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RaRays implements ModInitializer {
+    public static final String MOD_ID = "rarays";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
