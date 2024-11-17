8d69b36d9945b572256a9d18fc9c1ab7dbe330ce
IDoTheHax <hax@idothehax.com>
2024-11-17 17:32:14 +0100

    fixxed the commit not commiting

--- src/main/java/net/idothehax/rarays/item/RaRaySpawnerItem.java
+++ src/main/java/net/idothehax/rarays/item/RaRaySpawnerItem.java
@@ -2,6 +2,7 @@ package net.idothehax.rarays.item;
 
 import eu.pb4.polymer.core.api.item.SimplePolymerItem;
 import net.idothehax.rarays.RaRays;
+import net.idothehax.rarays.config.Config;
 import net.idothehax.rarays.laser.Laser;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
@@ -9,12 +10,11 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.ActionResult;
 import net.minecraft.world.World;
 
 public class RaRaySpawnerItem extends SimplePolymerItem {
@@ -23,13 +23,13 @@ public class RaRaySpawnerItem extends SimplePolymerItem {
     }
 
     @Override
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+    public ActionResult use(World world, PlayerEntity user, Hand hand) {
         ItemStack stack = user.getStackInHand(hand);
 
         // Check if the item is on cooldown, if so do nuthin
-        if (user.getItemCooldownManager().isCoolingDown(this)) {
+        if (user.getItemCooldownManager().isCoolingDown(stack)) {
             user.sendMessage(Text.literal("Your Ray is on cooldown!").formatted(Formatting.DARK_RED), false);
-            return TypedActionResult.pass(stack);
+            return ActionResult.PASS;
         }
 
         if (!world.isClient) {
@@ -38,7 +38,7 @@ public class RaRaySpawnerItem extends SimplePolymerItem {
                 user.addStatusEffect(new StatusEffectInstance(StatusEffects.GLOWING, 1200, 0, false, false, true));
             }
 
-            world.playSound((PlayerEntity)null, user.getX() + 0.5, user.getY() + 0.5, user.getZ() + 0.5, SoundEvents.BLOCK_RESPAWN_ANCHOR_CHARGE, SoundCategory.BLOCKS, 3.0F, 1.0F);
+            world.playSound(null, user.getX() + 0.5, user.getY() + 0.5, user.getZ() + 0.5, SoundEvents.BLOCK_RESPAWN_ANCHOR_CHARGE, SoundCategory.BLOCKS, 3.0F, 1.0F);
 
             // Spawn the laser
             Laser laser = new Laser(world, user);
@@ -46,12 +46,11 @@ public class RaRaySpawnerItem extends SimplePolymerItem {
             RaRays.lasers.add(laser);
 
             // Set the item on cooldown
-            user.getItemCooldownManager().set(this, 5000); // 5 minutes
+            user.getItemCooldownManager().set(stack, Config.getInstance().getRaRaysCooldown()); // 5 minutes
 
-            return TypedActionResult.success(stack);
+            return ActionResult.SUCCESS;
         }
 
-        return TypedActionResult.pass(stack);
+        return ActionResult.PASS;
     }
-
 }
