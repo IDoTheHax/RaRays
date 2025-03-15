67b30fba3b9b3beb53bf048f00123d26f4824bc1
IDoTheHax <hax@idothehax.com>
2025-03-15 11:02:26 +0100

    Updated Laser Ticker to split the despawn

--- src/main/java/net/idothehax/rarays/laser/LaserTicker.java
+++ src/main/java/net/idothehax/rarays/laser/LaserTicker.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class LaserTicker implements ServerTickEvents.EndTick {
     private static final List<Laser> activeLasers = new ArrayList<>();
+    private static final List<LaserExplosion> activeExplosions = new ArrayList<>();
     private static LaserTicker INSTANCE;
 
     private LaserTicker() {}
@@ -27,15 +28,30 @@ public class LaserTicker implements ServerTickEvents.EndTick {
         activeLasers.add(laser);
     }
 
+    public static void addExplosion(LaserExplosion explosion) {
+        activeExplosions.add(explosion);
+    }
+
     @Override
     public void onEndTick(MinecraftServer server) {
-        Iterator<Laser> iterator = activeLasers.iterator();
-        while (iterator.hasNext()) {
-            Laser laser = iterator.next();
+        // Update all active lasers
+        Iterator<Laser> laserIterator = activeLasers.iterator();
+        while (laserIterator.hasNext()) {
+            Laser laser = laserIterator.next();
             laser.update();
-            // Remove the laser if it's finished (you'll need to add this method to Laser class)
             if (laser.isFinished()) {
-                iterator.remove();
+                laserIterator.remove();
+            }
+        }
+
+        // Update all active explosions
+        Iterator<LaserExplosion> explosionIterator = activeExplosions.iterator();
+        while (explosionIterator.hasNext()) {
+            LaserExplosion explosion = explosionIterator.next();
+            explosion.update();
+            if (explosion.isFinished()) {
+                explosion.cleanup(); // Clean up resources
+                explosionIterator.remove(); // Remove finished explosion
             }
         }
     }
